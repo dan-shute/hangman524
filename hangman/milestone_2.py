49d75a3086d6757268908af14f03504d5d10e46c
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 22:05:23 +0100

    Imports the Random module, uses the random.choice method to choose a random word from our word_list and outputs the result.

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,4 +1,12 @@
-# Create 
+# Import modules
+import random
+
+# Create the fruit list
 word_list = ['passion fruit', 'strawberry', 'apple', 'banana', 'mango']
 
-print(word_list)
+# Use the random module to choice a word from the list at random.
+word = random.choice(word_list)
+
+# Print the selected word
+print(word)
+
