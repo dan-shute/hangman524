433f664d7018a03e7d766e6578d5c53c20083a9c
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 22:58:28 +0100

    Checks if the guessed letter is in the random word.

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -2,6 +2,13 @@
 # Import modules
 import random
 
+# Create the fruit list
+word_list = ['passion fruit', 'strawberry', 'apple', 'banana', 'mango']
+
+# Use the random module to choice a word from the list at random.
+word = random.choice(word_list)
+print(word)
+
 # Create a while loop that asks a user for an input, check if the input is valid.
 while True:
     # Get the user input
@@ -12,3 +19,9 @@ while True:
         break
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
+
+# Check if the letter is in the random word.
+if guess in word:
+    print(f'Good guess! {guess} is in the word.')
+else:
+    print(f'Sorry, {guess} is not in the word. Try again.')
