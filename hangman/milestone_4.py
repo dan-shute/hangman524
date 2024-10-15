5afd4e63f8e04a90642c64a9b219ff722de1c074
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 23:42:33 +0100

    Creates the Hangman class and initialise the attributes

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -2,3 +2,15 @@
 # Import modules
 import random
 
+# Create the Hangman class
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        #attributes
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_','_','_','_','_']
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = list()
+
+
