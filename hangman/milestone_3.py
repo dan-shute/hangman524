a66e5f27ed6b8ee8518ab4dc40fd3299a4db6826
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 22:52:37 +0100

    Adds a loop that will ask the user for an input. The loop checks if the input is valid and only ends when it is valid.

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -2,3 +2,13 @@
 # Import modules
 import random
 
+# Create a while loop that asks a user for an input, check if the input is valid.
+while True:
+    # Get the user input
+    guess = input('Please enter a single letter guess: ')
+
+    # Check if the input meets the valid criteria
+    if len(guess) == 1 and guess.isalpha() == True:
+        break
+    else:
+        print('Invalid letter. Please, enter a single alphabetical character.')
