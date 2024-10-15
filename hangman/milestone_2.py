ac6271e950779f072dfccb1f3ef6a9b0201047e1
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 22:08:48 +0100

    Adds a variable called 'guess' which is assigned to the user input. Where the user is asked to enter a single letter.

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -10,3 +10,5 @@ word = random.choice(word_list)
 # Print the selected word
 print(word)
 
+# Ask the user to input a single letter, place this input into the variable 'guess'
+guess = input('Please enter a single letter: ')
