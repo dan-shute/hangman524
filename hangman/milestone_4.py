e843f04cf1cb1af62b61dcf051cd9ecdbee3e175
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 23:46:44 +0100

    Adds comments to the attributes and fixes definition of word_guessed attribute.

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -6,11 +6,13 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
         #attributes
-        self.word = random.choice(word_list)
-        self.word_guessed = ['_','_','_','_','_']
-        self.num_letters = len(set(self.word))
-        self.num_lives = num_lives
-        self.word_list = word_list
-        self.list_of_guesses = list()
+        self.word = random.choice(word_list) # Chosen random word
+        self.word_guessed = ['_']*len(self.word) # Letters of the word guessed
+        self.num_letters = len(set(self.word)) # Number of unique letters in the word that have not been guessed
+        self.num_lives = num_lives # Number of lives
+        self.word_list = word_list # Word list
+        self.list_of_guesses = list() # List of guessed characters.
+
+    
 
 
