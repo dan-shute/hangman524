d7a5e3d3f8e9e46318f3753c060d72435a56dff3
Dan (Laptop) <danshute@hotmail.co.uk>
2024-10-15 22:22:49 +0100

    Adds a condition that checks if the user input is a valid letter and only one character long. Prints feedback depending whether the condition is met or not

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -12,3 +12,9 @@ print(word)
 
 # Ask the user to input a single letter, place this input into the variable 'guess'
 guess = input('Please enter a single letter: ')
+
+# Check that the input is only one character long and that it is an alphabetical letter.
+if len(guess) == 1 and guess.isalpha() == True:
+    print('Good Guess!')
+else:
+    print('Oops! That is not a valid input.')
